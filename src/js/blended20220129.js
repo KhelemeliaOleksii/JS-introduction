dc799297a66eadf65517bf9a67920c6cc8ec0474
KhelemeliaOleksii <xvdm@ukr.net>
2022-02-02 08:34:40 +0200

    add new materials
    blended course lesson
    add tests list

--- src/js/blended20220129.js
+++ src/js/blended20220129.js
@@ -78,7 +78,7 @@ const people4 = [{
 },
 ]; 
 //немає нарциса'
-
+/* 
 function searchSelebrity (argu) {
     for (let i = 0; i < argu.length; i += 1) {
         argu[i].knowMe = 0;
@@ -90,36 +90,42 @@ function searchSelebrity (argu) {
         }
     }
     for( let i = 0; i < argu.length; i += 1) {
-        if (argu[i].knowMe === (argu.length-1)) {
+        if (argu[i].knowMe === (argu.length-1) && argu[i].know.length === 0) {
             return argu[i].name; 
         }
     }
+
     return 'no one';
 }
+console.log(searchSelebrity(people1));
+console.log(searchSelebrity(people2));
+console.log(searchSelebrity(people3));
+console.log(searchSelebrity(people4));
+ */
 
-function searchSelebrity1 (argu) {
+
+function searchCelebrity1 (argu) {
     //спробуй через filter i every
-    //let amISelebrity = false;
-    const array =  argu.filter((item, index, arr) => { 
-            arr.every((it) => {
-                if (it === item) {
-                    return true;
-                } else {
-                    console.log(argu, it.know.includes(item.name));
-                    return it.know.includes(item.name);
-                } 
-
-                console.log(tmp);
-            //console.log(it.know.includes(item.name));
-            });
-        // return arr.every((it) => it.know.includes(item))
-        });
-    //console.log(array);
+    //let amICelebrity = false;
+    const arrayCandidate = argu.filter((item)=>item.know.length === 0);
+    if (arrayCandidate.length !== 1) {
+        return 'No celebrity';
+    }
+    const isCandidateCelebrity = argu.every((it, indx)=> {
+                        if (it.name === arrayCandidate[0].name) {
+                            return true;
+                        }
+                        return it.know.includes(arrayCandidate[0].name);
+                    }); 
+    if (!isCandidateCelebrity){
+        return 'No celebrity';
+    }
+    return  arrayCandidate[0].name;
 }
 
-console.log(searchSelebrity1(people1));
-console.log(searchSelebrity1(people2));
-console.log(searchSelebrity1(people3));
-console.log(searchSelebrity1(people4));
+console.log(searchCelebrity1(people1));
+console.log(searchCelebrity1(people2));
+console.log(searchCelebrity1(people3));
+console.log(searchCelebrity1(people4));
 
 
